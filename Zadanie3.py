f10a540f2797cd1380e0d334e0e644c6cb61f9c4
Filemon3Q <183423834+Filemon3Q@users.noreply.github.com>
2024-10-05 13:18:25 +0200

    Delete Zadanie3.py

--- Zadanie3.py
+++
@@ -1,9 +0,0 @@
-def krzyzyk(n):
-    for i in range(n):
-        print(' ' * n + '*' * n)
-    for i in range(n):
-        print('*' * (n * 3))
-    for i in range(n):
-        print(' ' * n + '*' * n)
-
-krzyzyk(4)
