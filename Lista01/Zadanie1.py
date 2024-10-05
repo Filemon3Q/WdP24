8f97b84ee7571aa8d81be816455aff944bc63e5a
Filemon3Q <183423834+Filemon3Q@users.noreply.github.com>
2024-10-05 15:56:33 +0200

    Add files via upload

---
+++ Lista01/Zadanie1.py
@@ -0,0 +1,38 @@
+from math import sqrt
+
+
+def potega(a,n):
+    wynik = 1
+    for i in range(n):
+       wynik = wynik * a
+    return wynik
+   
+def kwadrat(n):
+    for i in range(n):
+       for j in range(n): 
+          print ("*", end="")
+       print()
+      
+def kwadrat2(n):
+    for i in range(n):
+       print (n * "#")      
+
+# Wycięcie kodu niepotrzebnego w
+# rozwiązaniu zadania.
+'''
+for i in range(10):
+    print (i, 2 ** i, potega(2,i), sqrt(i))  # drukujemy kolejne liczby wraz z kolejnymi potęgami dwójki oraz kolejnymi pierwiastkami                  
+print ()
+'''
+
+# Kwadraty z gwiazdek do 4 (zamiast liczb
+# parzystych), a z hasztagów do 9 (+ zmniejszenie
+# o 5 wartości 'i' w argumencie.)
+for i in range(10):
+    print ("Przebieg:",i)
+    print (20 * "-")
+    if i <= 4:
+        kwadrat(3+2*i)
+    else:
+        kwadrat2(3 + (i - 5))
+    print()
